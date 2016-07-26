e92a255cc58486e9db915eeb722252e548a430ce
Yin Qi <qiyin@zplay.com>
2016-07-26 17:29:55 +0800

    解决nginx静态文件路径问题

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 header("Content-type: text/html; charset=utf-8");
 define('ROOT_PATH', __DIR__.'/');
-define('STATIC_PATH', dirname($_SERVER["PHP_SELF"]).'/static/');
+define('STATIC_PATH', rtrim(dirname($_SERVER["PHP_SELF"]), "/").'/static/');
 
 $config = require dirname(__FILE__) . '/config.php';
 require_once dirname(__FILE__) . '/lib/Dbver.php';
