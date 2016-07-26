88cf8e6dee40ef6cb596450dabd8fdf3156d8036
Yin Qi <qiyin@zplay.com>
2016-07-26 11:07:07 +0800

    优化视图展示

--- lib/View.php
+++ lib/View.php
@@ -2,26 +2,34 @@
 
 namespace lib;
 
-class View 
+class View
 {
     protected $var;
-    
-    public function display($fileName)
+
+    public function display($fileName, $isReturn = false)
     {
-        include T.$fileName.'.php';
+        if ($isReturn) {
+            ob_start();
+            include T . $fileName . '.php';
+            $content = ob_get_contents();
+            ob_clean();
+            return $content;
+        }
+
+        include T . $fileName . '.php';
         exit;
     }
-    
+
     public function import($fileName)
     {
-        include T.$fileName.'.php';
+        include T . $fileName . '.php';
     }
-    
+
     public function __get($name)
     {
         return $this->var[$name];
     }
-    
+
     public function __set($name, $value)
     {
         $this->var[$name] = $value;
